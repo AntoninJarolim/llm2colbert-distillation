e0353689e40708fedf87c56aac04c9c0c56a51c2
Antonín Jarolím <jarolim.antonin@seznam.cz>
2024-12-14 19:35:52 +0100

    Benchmark: add input/output arguments

--- 02_quantization_benchmark.sh
+++ 02_quantization_benchmark.sh
@@ -10,20 +10,27 @@ fi
 
 # Define model names and common arguments
 models=(
- "gemma2:27b-instruct-q8_0 --use_ollama"
- "gemma2:27b-instruct-fp16 --use_ollama"
+ "gemma2:27b-instruct-q8_0"
+ "gemma2:27b-instruct-fp16"
 )
 
 # Iterate over the models
-for model_args in "${models[@]}"; do
-  echo "Running benchmark for model: $model_args"
+for model_name in "${models[@]}"; do
+  echo "Running benchmark for model: $model_name"
   start_time=$(date +%s)  # Record the start time
 
-  python 01a_gpt_explanations.py --model_name $model_args --batch_size 15 --to_sample 500 --force_rewrite
+  python 01a_gpt_explanations.py \
+    --model_name $model_name \
+    --batch_size 15 \
+    --to_sample 500 \
+    --force_rewrite \
+    --use_ollama \
+    --input_data_name "data/bclavie-msmarco-2m-triplets/train-00000-of-00003.jsonl" \
+    --output_data_name "${model_name}.jsonl"
 
   end_time=$(date +%s)  # Record the end time
   runtime=$((end_time - start_time))  # Calculate runtime
-  echo "Runtime for model ($model_args): ${runtime}s"
+  echo "Runtime for model ($model_name): ${runtime}s"
   echo -e "\n\n"
 done
 
